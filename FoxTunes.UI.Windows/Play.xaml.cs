f9df14171d0aec438bbcdd5787b153b20aaeafbd
pudding <sophie@raimusoft.com>
2026-04-18 17:23:39 +0100

    chore: make play control uicomponent

--- FoxTunes.UI.Windows/Play.xaml.cs
+++ FoxTunes.UI.Windows/Play.xaml.cs
@@ -3,6 +3,7 @@
     /// <summary>
     /// Interaction logic for Play.xaml
     /// </summary>
+    [UIComponent("705CFB8A-D704-4D12-B8B6-7576EC431F33", role: UIComponentRole.Playback)]
     public partial class Play : UIComponentBase
     {
         public Play()
