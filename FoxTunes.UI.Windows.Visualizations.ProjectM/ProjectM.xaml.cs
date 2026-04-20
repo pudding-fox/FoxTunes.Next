af3f097824e4b121e90edc256a57d614e1d79bf7
pudding <sophie@raimusoft.com>
2026-04-20 21:52:24 +0100

    wip: begin stubbing out projectm visualizer

---
+++ FoxTunes.UI.Windows.Visualizations.ProjectM/ProjectM.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace FoxTunes
+{
+    /// <summary>
+    /// Interaction logic for ProjectM.xaml
+    /// </summary>
+    [UIComponent("D3FBE95D-3B9E-4DAB-B3AD-B66A53AF5F85", role: UIComponentRole.Visualization)]
+    public partial class ProjectM : UIComponentBase
+    {
+        public const string CATEGORY = "15AC0D00-4359-4DC9-941D-42803AB999DE";
+
+        public ProjectM()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
