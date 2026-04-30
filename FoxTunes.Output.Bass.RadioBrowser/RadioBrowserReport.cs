1fd5b7e9ba9ca2dbdd0468e6e5a08b5c7754f6e7
pudding <sophiesticated.fox@gmail.com>
2026-04-30 11:32:37 +0100

    chore: refactoring

---
+++ FoxTunes.Output.Bass.RadioBrowser/RadioBrowserReport.cs
@@ -0,0 +1,111 @@
+﻿using FoxTunes.Interfaces;
+using RadioBrowserWrapper.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FoxTunes
+{
+    public class RadioBrowserReport : ReportComponent
+    {
+        public RadioBrowserReport(RadioBrowserBehaviour behaviour, IEnumerable<Station> stations)
+        {
+            this.Behaviour = behaviour;
+            this.Stations = stations;
+        }
+
+        public RadioBrowserBehaviour Behaviour { get; private set; }
+
+        public IEnumerable<Station> Stations { get; private set; }
+
+        public override string Title
+        {
+            get
+            {
+                return Strings.RadioBrowserBehaviour_Path;
+            }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return string.Empty;
+            }
+        }
+
+        public override string[] Headers
+        {
+            get
+            {
+                return new[]
+                {
+                        nameof(Station.Name),
+                        nameof(Station.UrlResolved)
+                    };
+            }
+        }
+
+        public override IEnumerable<IReportComponentRow> Rows
+        {
+            get
+            {
+                foreach (var station in this.Stations)
+                {
+                    yield return new RadioBrowserReportRow(this, station);
+                }
+            }
+        }
+
+        public class RadioBrowserReportRow : ReportComponentRow
+        {
+            public RadioBrowserReportRow(RadioBrowserReport report, Station station)
+            {
+                this.Report = report;
+                this.Station = station;
+            }
+
+            public RadioBrowserReport Report { get; private set; }
+
+            public Station Station { get; private set; }
+
+            public override string[] Values
+            {
+                get
+                {
+                    return new[]
+                    {
+                            this.Station.Name,
+                            this.Station.UrlResolved
+                        };
+                }
+            }
+
+
+            public override IEnumerable<string> InvocationCategories
+            {
+                get
+                {
+                    yield return InvocationComponent.CATEGORY_REPORT;
+                }
+            }
+
+            public override IEnumerable<IInvocationComponent> Invocations
+            {
+                get
+                {
+                    yield return new InvocationComponent(InvocationComponent.CATEGORY_REPORT, ACTIVATE, attributes: InvocationComponent.ATTRIBUTE_SYSTEM);
+                }
+            }
+
+            public override Task InvokeAsync(IInvocationComponent component)
+            {
+                switch (component.Id)
+                {
+                    case ACTIVATE:
+                        return this.Report.Behaviour.AddStationsToPlaylist(new[] { this.Station });
+                }
+                return base.InvokeAsync(component);
+            }
+        }
+    }
+}
---
+++ FoxTunes.Output.Bass.RadioBrowser/Tasks/AddStationsToPlaylistTask.cs
@@ -0,0 +1,111 @@
+﻿using FoxDb;
+using FoxDb.Interfaces;
+using FoxTunes.Interfaces;
+using RadioBrowserWrapper.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FoxTunes
+{
+    public class AddStationsToPlaylistTask : PlaylistTaskBase
+    {
+        public AddStationsToPlaylistTask(Playlist playlist, int sequence, IEnumerable<Station> stations, bool clear) : base(playlist, sequence)
+        {
+            this.Stations = stations;
+            this.Clear = clear;
+        }
+
+        public override bool Visible
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public IEnumerable<Station> Stations { get; private set; }
+
+        public bool Clear { get; private set; }
+
+        public IPlaylistBrowser PlaylistBrowser { get; private set; }
+
+        public override void InitializeComponent(ICore core)
+        {
+            this.PlaylistBrowser = core.Components.PlaylistBrowser;
+            base.InitializeComponent(core);
+        }
+
+        protected override Task OnStarted()
+        {
+            this.Name = "Opening archive";
+            return base.OnStarted();
+        }
+
+        protected override async Task OnRun()
+        {
+            if (this.Clear)
+            {
+                await this.RemoveItems(PlaylistItemStatus.None).ConfigureAwait(false);
+            }
+            await this.AddStations(this.Stations, false).ConfigureAwait(false);
+        }
+
+        protected virtual async Task<int> AddStations(IEnumerable<Station> stations, bool maintainOrder)
+        {
+            var count = default(int);
+            using (var task = new SingletonReentrantTask(this, ComponentSlots.Database, SingletonReentrantTask.PRIORITY_HIGH, async cancellationToken =>
+            {
+                count = await this.AddPlaylistItems(stations, cancellationToken).ConfigureAwait(false);
+                await this.ShiftItems(QueryOperator.GreaterOrEqual, this.Sequence, this.Offset).ConfigureAwait(false);
+                await this.SequenceItems(maintainOrder).ConfigureAwait(false);
+                await this.SetPlaylistItemsStatus(PlaylistItemStatus.None).ConfigureAwait(false);
+            }))
+            {
+                await task.Run().ConfigureAwait(false);
+            }
+            return count;
+        }
+
+        protected virtual async Task<int> AddPlaylistItems(IEnumerable<Station> stations, CancellationToken cancellationToken)
+        {
+            var count = default(int);
+            var playlistItems = this.CreatePlaylistItems(stations);
+            using (var transaction = this.Database.BeginTransaction(this.Database.PreferredIsolationLevel))
+            {
+                var set = this.Database.Set<PlaylistItem>(transaction);
+                foreach (var playlistItem in playlistItems)
+                {
+                    Logger.Write(this, LogLevel.Debug, "Adding file to playlist: {0}", playlistItem.FileName);
+                    playlistItem.Playlist_Id = this.Playlist.Id;
+                    playlistItem.Sequence = this.Sequence;
+                    playlistItem.Status = PlaylistItemStatus.Import;
+                    await set.AddAsync(playlistItem).ConfigureAwait(false);
+                    count++;
+                    this.Offset++;
+                }
+                if (transaction.HasTransaction)
+                {
+                    transaction.Commit();
+                }
+            }
+            return count;
+        }
+
+        protected virtual IEnumerable<PlaylistItem> CreatePlaylistItems(IEnumerable<Station> stations)
+        {
+            foreach (var station in stations)
+            {
+                yield return new PlaylistItem()
+                {
+                    DirectoryName = station.UrlResolved,
+                    FileName = station.UrlResolved
+                };
+            }
+        }
+
+        protected override Task OnCompleted()
+        {
+            return this.SignalEmitter.Send(new Signal(this, CommonSignals.PlaylistUpdated, new PlaylistUpdatedSignalState(this.Playlist, DataSignalType.Updated)));
+        }
+    }
+}
