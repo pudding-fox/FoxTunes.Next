333015406054df29068c1d364d121ebdbbfa9875
pudding <pudding@raimusoft.com>
2026-05-16 01:43:26 +0100

    feat: download artist images in the background

---
+++ FoxTunes.UI.Windows.LibraryBrowser/Behaviours/LibraryBrowserBehaviour.cs
@@ -0,0 +1,116 @@
+﻿using FoxTunes.Interfaces;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoxTunes
+{
+    public class LibraryBrowserBehaviour : StandardBehaviour, IDisposable
+    {
+        public ILibraryHierarchyBrowser LibraryHierarchyBrowser { get; private set; }
+
+        public IOnDemandMetaDataProvider OnDemandMetaDataProvider { get; private set; }
+
+        public ISignalEmitter SignalEmitter { get; private set; }
+
+        public override void InitializeComponent(ICore core)
+        {
+            this.LibraryHierarchyBrowser = core.Components.LibraryHierarchyBrowser;
+            this.OnDemandMetaDataProvider = core.Components.OnDemandMetaDataProvider;
+            this.SignalEmitter = core.Components.SignalEmitter;
+            this.SignalEmitter.Signal += this.OnSignal;
+            base.InitializeComponent(core);
+        }
+
+        protected virtual Task OnSignal(object sender, ISignal signal)
+        {
+            switch (signal.Name)
+            {
+                case CommonSignals.HierarchiesUpdated:
+                    return this.Refresh();
+            }
+#if NET40
+            return TaskEx.FromResult(false);
+#else
+            return Task.CompletedTask;
+#endif
+        }
+
+        protected virtual async Task Refresh()
+        {
+            foreach (var libraryHierarchy in this.LibraryHierarchyBrowser.GetHierarchies())
+            {
+                var libraryHierarchyNodes = this.LibraryHierarchyBrowser.GetNodes(libraryHierarchy);
+                foreach (var libraryHierarchyNode in libraryHierarchyNodes)
+                {
+                    var libraryHierarchyLevel = this.LibraryHierarchyBrowser.GetLevel(libraryHierarchyNode.LibraryHierarchyLevelId.Value);
+                    if (libraryHierarchyLevel != null)
+                    {
+                        switch (libraryHierarchyLevel.Hints)
+                        {
+                            case LibraryHierarchyLevelHints.Artist:
+                                var skip = new[]
+                                {
+                                    "No Artist",
+                                    "Various Artists"
+                                };
+                                if (!skip.Any(_skip => string.Equals(libraryHierarchyNode.Value, _skip, StringComparison.OrdinalIgnoreCase)))
+                                {
+                                    var libraryItems = this.LibraryHierarchyBrowser.GetItems(libraryHierarchyNode);
+                                    await this.OnDemandMetaDataProvider.GetMetaData(
+                                        libraryItems,
+                                        new OnDemandMetaDataRequest(
+                                            CommonImageTypes.Artist,
+                                            MetaDataItemType.Image,
+                                            MetaDataUpdateType.System
+                                        )
+                                    ).ConfigureAwait(false);
+                                }
+                                await Task.Delay(100).ConfigureAwait(false);
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public bool IsDisposed { get; private set; }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this.IsDisposed || !disposing)
+            {
+                return;
+            }
+            this.OnDisposing();
+            this.IsDisposed = true;
+        }
+
+        protected virtual void OnDisposing()
+        {
+            if (this.SignalEmitter != null)
+            {
+                this.SignalEmitter.Signal -= this.OnSignal;
+            }
+        }
+
+        ~LibraryBrowserBehaviour()
+        {
+            Logger.Write(this, LogLevel.Error, "Component was not disposed: {0}", this.GetType().Name);
+            try
+            {
+                this.Dispose(true);
+            }
+            catch
+            {
+                //Nothing can be done, never throw on GC thread.
+            }
+        }
+    }
+}
